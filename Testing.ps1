4c12f025cd3570f017ce6ee89b1ac03de8839819
Petter Bomban <petterbomban@gmail.com>
2017-02-20 12:58:49 +0100

    Update to test

--- Testing.ps1
+++ Testing.ps1
@@ -1,7 +1,19 @@
+param
+(
+    [PScredential]$credential
+)
+
 Remove-Module PoShWave
 Import-Module .\PoShWave.psm1
 
-$cred = Get-Credential
-$con = Connect-AirWave -Api "https://900-araw-01.akershus-fk.no" -Credential $cred
+if(!($credential))
+{
+    $credential = Get-Credential
+}
+$con = Connect-AirWave -Api "https://900-araw-01.akershus-fk.no" -Credential $credential
 
-$con | Invoke-AirWaveQuery -Query "Ap List" -Verbose
+# ApList
+$con | Get-ApList -Id 533 -Verbose
+
+# Amp Status
+#$con | Get-AmpStats -Verbose
