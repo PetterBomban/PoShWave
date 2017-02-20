7d5e4bdacf854d0b7ce6e6a27086bab42a07e113
Petter Bomban <petterbomban@gmail.com>
2017-02-20 12:58:44 +0100

    Starting to add API params

--- Public/Get-ApList.ps1
+++ Public/Get-ApList.ps1
@@ -5,15 +5,28 @@ function Get-ApList
     (
         [Parameter( Mandatory = $True,
                     Position = 0,
-                    ValueFromPipeline = $True)]
-        $Session
+                    ValueFromPipeline = $True )]
+        [PSCustomObject]$Session,
+
+        [Parameter( Mandatory = $False,
+                    Position = 1 )]
+        [Int]$Id
     )
 
     Write-Verbose "Getting Ap List.."
-
-    $Uri = $Session.Api + "/ap_list.xml"
+    $Uri = $Session.Api + "/ap_list.xml?"
+    foreach ($Param in $PSBoundParameters.GetEnumerator())
+    {
+        $Key = ($Param.Key).toLower()
+        $Value = $Param.Value
+        ## TODO: Better way to do this?
+        if ($Key -ne "id") { continue }
+        ## Construct links like so:
+        ## $Api/ap_list.xml?id=123&anotherKey=2& etc..
+        $Uri += ($Key + "=" + $Value + "&")
+    }
+    Write-Verbose "Constructed URI: $Uri"
     
     $Response = Invoke-WebRequest -Uri $Uri -WebSession $Session.Session -Method Get
-    [xml]$Response = $Response.Content
-    $Response
+    [xml]$Response.Content
 }
