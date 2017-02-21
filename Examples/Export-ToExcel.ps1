a161974eb7bd8ed6a8aa9321f79b9e5d5dc5d5e6
Petter Bomban <petterbomban@gmail.com>
2017-02-21 11:47:48 +0100

    Move to Examples folder

--- Examples/Export-ToExcel.ps1
+++ Examples/Export-ToExcel.ps1
@@ -11,17 +11,19 @@ param
 )
 
 Remove-Module PoShWave
-Import-Module .\PoShWave.psm1
+Import-Module ..\PoShWave.psm1
 
 $con = Connect-AirWave -Api "https://900-araw-01.akershus-fk.no" -Credential $credential
 
 $Devices = $con | Get-AirWaveDevice
-#$Switches = $Devices | Where-Object { $_.device_category -eq "switch" }
 $APs = $Devices | Where-Object { $_.device_category -like "*ap*" }
 $Switches = $Devices | Where-Object { $_.device_category -eq "switch" }
 
 ## ArrayList to hold already visited AP's
 $Visited = New-Object System.Collections.ArrayList
+
+## Gathering all of them by the switch name
+$Collection = @{}
 
 foreach ($AP in $APs)
 {
@@ -29,21 +31,22 @@ foreach ($AP in $APs)
     $ApConnectedTo = $AP.upstream_device_id
     $ApName = $AP.name
     $ApIp = $AP.lan_ip
+
     $Switch = $Switches | Where-Object { $_.id -eq $ApConnectedTo }
-
-    ## We've already visited this AP.
-    if ($Visited.Contains($ApIp)) { continue }
-
     ## Quick and dirty to avoid errors
-    if ($Switch.name -eq $null) { $Switch = [PSCustomObject]@{ name = @("NOT_CONNECTED")}}
-    
+    if ($Switch.name -eq $null) { $Switch = [PSCustomObject]@{ name = @("_UNKNOWN")}}
+    $SwitchName = ($Switch.name)[0]
+
+    ## Skip already visited APs (since the api returns one several times..?)
+    if ($Visited.Contains($ApIp)) { continue }
     [void]$Visited.Add($ApIp)
 
     $obj = [PSCustomObject]@{
-        Switch = ($Switch.name)[0] ## somehow always returns two of the same switch..
-        SwitchApPort = $ApPort
         ApName = $ApName
         ApIp = $ApIp
+        SwitchApPort = $ApPort
     }
-    $obj
+    $Collection[$SwitchName] += @($obj)
 }
+
+$Collection
