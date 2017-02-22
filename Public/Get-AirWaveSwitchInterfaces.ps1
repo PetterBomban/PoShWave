a08e76d9204ccf9b6bd69340c96f88ceceda9085
Petter Bomban <petterbomban@gmail.com>
2017-02-22 15:14:56 +0100

    Get-AirWaveSwitchInterfaces now ordered by port

--- Public/Get-AirWaveSwitchInterfaces.ps1
+++ Public/Get-AirWaveSwitchInterfaces.ps1
@@ -15,12 +15,31 @@ function Get-AirWaveSwitchInterfaces
     ## Path to the .csv-file of the gived switch's interfaces
     $Uri = "{0}/nf/csv_export.csv?csv_export_uri=%2Finterface_list&csv_export_list_namespace=aruba_physical_interfaces&csv_export_list_args=ap_id%3D{1}" -f $Session.Api, $Id
     Write-Verbose "Sending request to: $Uri"
-    ## Store the output csv in TEMP
+
     $Date = Get-Date -Format dd-MM-yyy
-    $Outfile = Join-Path -Path $ENV:TEMP -ChildPath "$Id_$Date"
+    $Outfile = Join-Path -Path $ENV:TEMP -ChildPath "$Id _ $Date.csv"
     Invoke-WebRequest -Uri $Uri -WebSession $Session.Session -OutFile $Outfile
 
-    $CsvFile = Get-Content -Path $Outfile
-    $Csv = $CsvFile | ConvertFrom-Csv -Delimiter ","
-    $Csv
+    $Csv = Import-Csv -Path $Outfile -Delimiter ","
+
+    ## Ordering the switchports
+    $OrderedCollection = @()
+    $PortNumber = $Csv.Count
+    for ($i = 0; $i -le $PortNumber; $i++)
+    {
+        foreach ($Switchport in $Csv)
+        {
+            $CurrPort = $Switchport.Interface
+            $CurrPort = $CurrPort.Split("/")[1] + "/" + $CurrPort.Split("/")[2]
+            
+            ## If the switch only contains "/", it's the mgmt-port
+            if ($CurrPort -eq "/") { $CurrPort = "mgmt" }
+            
+            if ($CurrPort -eq "0/$i" -or $CurrPort -eq "1/$i")
+            {
+                $OrderedCollection += $Switchport
+            }
+        }
+    }
+    $OrderedCollection
 }
