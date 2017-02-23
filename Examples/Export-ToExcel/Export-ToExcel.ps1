716b944e6ed5cae0bb25233bed6744b504d95e3d
Petter Bomban <petterbomban@gmail.com>
2017-02-23 14:46:27 +0100

    A lot of changes

--- Examples/Export-ToExcel/Export-ToExcel.ps1
+++ Examples/Export-ToExcel/Export-ToExcel.ps1
@@ -4,7 +4,7 @@ param
 )
 
 Remove-Module PoShWave -ErrorAction SilentlyContinue
-Import-Module "C:\Users\admin\Documents\GitHub\PoShWave\PoShWave.psm1", ImportExcel
+Import-Module "C:\Users\admin\Documents\GitHub\PoShWave\PoShWave.psm1" #, ImportExcel
 
 $con = Connect-AirWave -Api "https://900-araw-01.akershus-fk.no" -Credential $credential
 
@@ -37,6 +37,126 @@ function Get-SwitchInterfaces
     $Collection
 }
 
+function Get-APsWithSwitches
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter( Mandatory = $True,
+                    Position = 0,
+                    ValueFromPipeline = $True)]
+        [PSCustomObject] $Session
+    )
+
+    $Devices = $Session | Get-AirWaveDevice 
+    $Switches = $Devices | Where-Object 'device_category' -eq "switch"
+    $APs = $Devices | Where-Object 'device_category' -like "*ap*"
+
+    ## Gather access points that we have already looped through
+    ## (AirWave some times returns several of the same Ap..)
+    $VisitedAps = New-Object System.Collections.ArrayList
+
+    ## Gather all AP's, grouped by which switch they are connected to
+    $Collection = @{}
+
+    foreach ($Ap in $APs)
+    {
+        ## Skip already visited (looped through) AccessPoints
+        if ($VisitedAps.Contains($Ap.lan_ip)) { continue }
+        [Void]$VisitedAps.Add($Ap.lan_ip)
+
+        ## AP variables
+        [int]$ApSwitchPort = $Ap.upstream_port_index
+        $ApConnectedTo = $Ap.upstream_device_id
+        $ApName = $Ap.name
+        $ApIp = $Ap.lan_ip
+        $ApMac = $Ap.lan_mac
+        $ApSerial = $Ap.serial_number
+
+        ## Switch variables
+        $SwitchConnected = $Switches | Where-Object id -eq $ApConnectedTo
+        ## Quick and dirty to avoid errors
+        if ($SwitchConnected.name -eq $null)
+        {
+            $SwitchConnected = [PSCustomObject]@{ name = "_UNKNOWN"}
+        }
+        $SwitchName = $SwitchConnected.name
+        $SwitchIp = $SwitchConnected.lan_ip
+        $SwitchMac = $SwitchConnected.lan_mac
+        $SwitchSerial = $SwitchConnected.serial_number
+        $SwitchId = $SwitchConnected.id
+
+        $Obj = [PSCustomObject]@{
+            SwitchId = $SwitchId
+            SwitchName = $SwitchName
+            SwitchIp = $SwitchIp
+            SwitchMac = $SwitchMac
+            SwitchSerial = $SwitchSerial
+            ApConnectedTo = $ApConnectedTo
+            ApName = $ApName
+            ApIp = $ApIp
+            ApMac = $ApMac
+            ApSerial = $ApSerial
+        }
+        $Collection.SwitchName
+        $Collection[$SwitchName] += @($Obj)
+    }
+    $Collection
+}
+
+# $con | Get-SwitchInterfaces | Add-APsToSwitches $APs | Export-ToExcel ...
+function Add-APsToSwitches
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter( Mandatory = $True,
+                    Position = 0)]
+        [PSCustomObject] $APsAndSwitches,
+
+        [Parameter( Mandatory = $True,
+                    Position = 1)]
+        [PSCustomObject] $SwitchPorts
+    )
+
+    ## Collecting switches with AP's in an easier format to work with
+    ## TODO: Fix this "upstream"!
+    $SwitchWithAPs = @()
+    $APsAndSwitches.Keys | ForEach-Object {
+        $SwitchName = $PSItem
+        $AccessPoints = $APsAndSwitches.$SwitchName
+
+        $Obj = [PSCustomObject]@{
+            SwitchName = $SwitchName
+            AccessPoints = $AccessPoints
+        }
+        $SwitchWithAPs += @($Obj)
+    }
+
+    ## Now we go through each switch with its switch ports.
+    ## Here we need to:
+    ## 1) Link a switch with switchports ($SwitchPort) with a switch with APs
+    ## 2) Find the interface where the AP is connected on both objects
+    ## 3) Append the AP-information to the correct switch port
+    ## 4) Output
+    foreach ($SwitchPort in $SwitchPorts.GetEnumerator())
+    {
+        if ($SwitchWithAPs.SwitchName -contains $SwitchPort.SwitchName)
+        {
+            $CurrSwitchWithAPs =
+                $SwitchWithAPs | Where-Object { $_.SwitchName -eq $SwitchPort.SwitchName }
+            Write-Debug $CurrSwitchWithAPs
+            
+            ## These two now match
+            $SwitchPort # Current SwitchPort Object, no APs
+            $CurrSwitchWithAPs ## Current Switch with AP
+
+            ## Next: Find out 
+        }
+        #>
+    }
+}
+
 function Export-ToExcel
 {
     [CmdletBinding()]
@@ -45,12 +165,67 @@ function Export-ToExcel
         [Parameter( Mandatory = $True,
                     Position = 0,
                     ValueFromPipeline = $True)]
-        $Collection
+        [PSCustomObject] $Collection,
+
+        [Parameter( Mandatory = $True,
+                    Position = 1)]
+        [String] $Path,
+
+        [Parameter( Mandatory = $False,
+                    Position = 2)]
+        [String] $DataMember,
+
+        [Parameter( Mandatory = $False,
+                    Position = 3)]
+        [Array] $Select = @()
     )
     process
     {
-        $Collection
+        ## Defining styles in the excel sheets
+        $CellStyleSB = {
+            param
+            (
+                $workSheet,
+                $totalRows,
+                $lastColumn
+            )
+
+            Set-CellStyle $workSheet 1 $lastColumn Solid SkyBlue
+
+            foreach ($row in (2..$totalRows | Where-Object { $_ % 2 -eq 0 }))
+            {
+                Set-CellStyle $workSheet $row $lastColumn Solid LightGray
+            }
+            foreach ($row in (2..$totalRows | Where-Object {  $_ % 2 -eq 1 }))
+            {
+                Set-CellStyle $workSheet $row $lastColumn Solid White
+            }
+        }
+        $ExportSplat = @{
+            Path = $Path
+            WorkSheetName = $Collection.SwitchName
+            CellStyleSB = $CellStyleSB
+            BoldTopRow = $True
+            Autosize = $True
+            FreezeTopRow = $True
+        }
+        ## Testing for $DataMember
+        if(!($Export = $Collection.$DataMember))
+        {
+            $Export = $Collection
+        }
+        $Export | Select-Object $Select #| Export-Excel @ExportSplat
     }
 }
 
-$con | Get-SwitchInterfaces | Export-ToExcel
+$Doc = "C:\Users\admin\Documents\GitHub\PoShWave\SwitchPorts.xlsx"
+$APs = $con | Get-APsWithSwitches
+$SwitchPorts = $con | Get-SwitchInterfaces
+
+$SelectSplat = @(
+
+)
+Add-APsToSwitches -APsAndSwitches $APs -SwitchPorts $SwitchPorts 
+#|Export-ToExcel -Path $Doc -Select $SelectSplat
+
+#$con | Get-SwitchInterfaces | Export-ToExcel -Path $SwitchPortsPath -DataMember "SwitchPorts"
